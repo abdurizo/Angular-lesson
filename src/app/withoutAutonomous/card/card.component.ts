a9c354c8747a4cfa716374604f106590fa487f2d
Abdurizo <103021939+abdurizo@users.noreply.github.com>
2025-10-29 17:54:03 +0500

    f

--- src/app/withoutAutonomous/card/card.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector:'cardwhiout',
-    templateUrl:'./card.component.html',
-    styleUrls:['./card.component.css'],
-})
-export class CardComponent{}
